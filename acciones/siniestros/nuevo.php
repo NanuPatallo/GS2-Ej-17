489f7536a0081476f2a092ef3e4a46f8b506c405
NanuPatallo <daianapatallo@gmail.com>
2022-09-29 23:13:58 -0300

    resol29092022

--- acciones/siniestros/nuevo.php
+++ acciones/siniestros/nuevo.php
@@ -10,11 +10,35 @@ require_once 'responses/nuevoResponse.php';
 $json = file_get_contents('php://input', true);
 $req = json_decode($json);
 
-$req = new NuevoRequest();
-
 $res = new NuevoResponse();
+$res->IsOk = true;
 
-if ($req->NroPoliza>1000&&$req->NroPoliza<0) {
-    $res->IsOk=false;
-    $res->Mensaje='La Póliza no existe.';
-} elseif () {} 
+
+if ($req->NroPoliza > 1000 or $req->NroPoliza < 0) {
+    $res->IsOk = false;
+    $res->Mensaje[] = 'La Póliza no existe.';
+} else {
+    if ($req->Vehiculo == null) {
+        $res->IsOk = false;
+        $res->Mensaje[] = 'Debe indicar el Vehículo';
+    } elseif (
+        $req->Vehiculo->Marca == null or $req->Vehiculo->Modelo == null or
+        $req->Vehiculo->Version == null or $req->Vehiculo->Anio == null
+    ) {
+        $res->IsOk = false;
+        $res->Mensaje[] = 'Debe indicar todas las propiedades del Vehículo';
+    }
+    if ($req->ListMediosContacto == null) {
+        $res->IsOk = false;
+        $res->Mensaje[] = 'Debe indicar al menos un Medio de Contacto';
+    } else {
+        foreach ($req->ListMediosContacto as $c) {
+            if ($c->MedioContactoDescripcion <> 'Celular' && $c->MedioContactoDescripcion <> 'Email') {
+                $res->IsOk = false;
+                $res->Mensaje[] = 'Debe indicar Medios de Contactos válidos';
+            }
+        }
+    }
+}
+
+echo json_encode($res);
---
+++ modelo/contacto.php
@@ -0,0 +1,7 @@
+<?php
+
+class ListMediosContacto
+{
+    public $MedioContactoDescripcion;
+    public $Valor;
+}
